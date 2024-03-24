85a9d51b11aaef4c2cc683e205e99650c2dc88d8
AntonioDehesa <hdehesaortiz@gmail.com>
2024-03-23 20:06:20 -0600

    Added image and basic color to the background

--- I_am_rich/lib/main.dart
+++ I_am_rich/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am rich"),
+          backgroundColor: Color(0xFF67BAFF),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202010/MIT-Metallic-Diamond-01-Press_0.jpg?itok=386hZmMI"),
+          ),
+        ),
       ),
     ),
   );
