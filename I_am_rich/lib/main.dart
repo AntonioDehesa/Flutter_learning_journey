04a252be30ba3a0900fa4d3fb8e997a35e8d4b97
AntonioDehesa <hdehesaortiz@gmail.com>
2024-03-25 23:04:04 -0600

    added images and icon

--- I_am_rich/lib/main.dart
+++ I_am_rich/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage(
-                "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202010/MIT-Metallic-Diamond-01-Press_0.jpg?itok=386hZmMI"),
+            image: AssetImage("images/diamond.png"),
           ),
         ),
       ),
