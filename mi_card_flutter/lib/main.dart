b9a4976c0c03c4d7523e3748ee38831850b0702b
AntonioDehesa <hdehesaortiz@gmail.com>
2024-04-03 22:30:41 -0600

    Almost completed the personal card app

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
@@ -18,17 +18,76 @@ class MyApp extends StatelessWidget {
             //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
                 backgroundColor: Colors.red,
                 foregroundImage: AssetImage("images/PFP.jpeg"),
               ),
-              Text(
+              const Text(
                 "Antonio Dehesa",
                 style: TextStyle(
+                    fontFamily: "Pacifico",
                     fontSize: 40.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "Technical Solutions Engineer",
+                style: TextStyle(
+                    fontFamily: "SourceSans3",
+                    fontSize: 20.0,
+                    color: Colors.greenAccent,
+                    fontWeight: FontWeight.w300),
+              ),
+              Container(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                padding: const EdgeInsets.all(10.0),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 50.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+52 123 456 789",
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: "SourceSans3",
+                          fontSize: 20.0),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                padding: const EdgeInsets.all(10.0),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 50.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "my_email@mail.com",
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: "SourceSans3",
+                          fontSize: 20.0),
+                    )
+                  ],
+                ),
               )
             ],
           ),
