ea24e0fdaeb5f905c2ec5573b96f13ca972b4c90
AntonioDehesa <hdehesaortiz@gmail.com>
2024-04-12 23:12:45 -0600

    started next section

---
+++ xylophone_flutter/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  XylophoneApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Center(
+          child: TextButton(
+              onPressed: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note1.wav'));
+              },
+              child: const Text("Click me")),
+        )),
+      ),
+    );
+  }
+}
