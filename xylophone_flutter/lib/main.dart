f5be577704c5d116ca8fab5aa65f37f35df432eb
AntonioDehesa <hdehesaortiz@gmail.com>
2024-04-22 23:37:00 -0600

    Almost completed section 9

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -2,24 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(XylophoneApp());
+  runApp(const XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
-  XylophoneApp({super.key});
+  const XylophoneApp({super.key});
+
+  void playSound(String audioName) {
+    final player = AudioPlayer();
+    player.play(AssetSource(audioName));
+  }
+
+  // According to flutter documentation, this is not the recommended approach. it is recommended to instead create a new widget and use that.
+  // but i am just following the course
+  Widget buildKey({required Color color, required int note}) {
+    return Expanded(
+      child: TextButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(color)),
+          onPressed: () {
+            playSound("note$note.wav");
+          },
+          child: const Text("Click me")),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child: Center(
-          child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: const Text("Click me")),
-        )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, note: 1),
+              buildKey(color: Colors.orange, note: 2),
+              buildKey(color: Colors.yellow, note: 3),
+              buildKey(color: Colors.green, note: 4),
+              buildKey(color: Colors.blue, note: 5),
+              buildKey(color: Colors.indigo, note: 6),
+              buildKey(color: Colors.purple, note: 7),
+            ],
+          ),
+        ),
       ),
     );
   }
