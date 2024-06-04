e6bf4635458c43c4a996ec4405c3139747b10e42
AntonioDehesa <hdehesaortiz@gmail.com>
2024-06-03 23:12:43 -0600

    Started next section

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+const reusableCardColor = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -15,16 +19,46 @@ class _InputPageState extends State<InputPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("BMI Calculator"),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Body Text',
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(child: ReusableCard()),
+              Expanded(child: ReusableCard())
+            ],
+          )),
+          Expanded(child: ReusableCard()),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(child: ReusableCard()),
+              Expanded(child: ReusableCard())
+            ],
+          )),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({super.key, this.color = reusableCardColor});
+
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), color: color),
     );
   }
 }
